7da3a301dcdcf6298ff42b248ddd2fcd1a495306
devlogex <tnd4698@gmail.com>
2020-11-01 12:03:09 +0700

    fix bug

---
+++ common/src/main/java/com/tnd/pw/development/common/requests/WorkspaceRequest.java
@@ -0,0 +1,7 @@
+package com.tnd.pw.development.common.requests;
+
+import com.tnd.common.api.common.base.authens.WorkspaceTokenRequest;
+
+public class WorkspaceRequest extends WorkspaceTokenRequest {
+
+}
